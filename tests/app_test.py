ac08fdd0b31b6d2411bd0a3066457dbe9283a6de
Lachlan Christie <lachlan.christie@linux.com>
2026-09-20 20:53:57 +1000

    update to split tests by functionality

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,13 +1,20 @@
 import os
 
-def test_form_data(filename: str, content: str) -> str:
-	return f"{filename}{content}"
+from flask import request
 
-def test_write():
+def test_data(result):
+	assert result == "general-note-tag3\nToday I learned..."
+
+def test_write(filename, content):
 	with open(filename, 'w') as f:
 		f.write(filename)
 		f.write(content)
 	with open(filename, 'r') as f:
 		result = f.read()
-	return result
+	return test_data(result)
+
+def test_get():
+	filename = request.form.get('tags')
+	content = request.form.get('content')
+	return test_write(filename, content)
 
